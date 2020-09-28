519ad0a9bcb6ceefd145277fd03c27bd77ad8cd9
Dmitry <destaom@gmail.com>
2020-09-28 23:51:31 +0300

    Get code country and allow any in imports tsconfig

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { MTProto } from '@mtproto/core'
+import countryTelephoneCode, { countries } from 'country-telephone-code'
+
+interface GeoInfo {
+  country: string
+}
 
 export const Auth = () => {
+  const [phone, setPhone] = useState('')
+
+  useEffect(() => {
+    const api_id = 1207761
+    const api_hash = '1acd94c546fd916fa25b73145be69da3'
+
+    // 1. Create an instance
+    const mtproto = new MTProto({
+      api_id,
+      api_hash,
+    })
+
+    // 3. Get the user country code
+    mtproto.call('help.getNearestDc', {}).then(result => {
+      const geo = result as GeoInfo
+
+      setPhone(`+${countryTelephoneCode(geo.country)}`)
+    })
+  }, [])
+
+  const onChangePhone = useCallback(e => {
+    e.preventDefault()
+    console.log('val: ', e.target.value)
+    setPhone(e.target.value)
+  }, [])
+
   return (
     <div className="auth-page">
       <div className="auth-center">
@@ -14,7 +46,13 @@ export const Auth = () => {
         <h1>Authorization</h1>
         <div className="auth-form">
           <div>
-            <input placeholder="Phone" type="text" />
+            <input
+              className="phone-auth-input"
+              placeholder="Phone"
+              type="text"
+              onChange={e => onChangePhone(e)}
+              value={phone}
+            />
           </div>
           <div className="next-button">
             Next <i className="material-icons next-button-icon">navigate_next</i>
