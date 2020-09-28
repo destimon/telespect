7f815722054ed8f411631820ec2ffdfc848a4a78
Dmitry Ch <dmitry.ch@inquire.dev>
2020-09-28 18:40:20 +0300

    Basic connection to mtproto telegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { MTProto } from '@mtproto/core'
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const api_id = 1207761;
+    const api_hash = '1acd94c546fd916fa25b73145be69da3';
+
+    // 1. Create an instance
+    const mtproto = new MTProto({
+      api_id,
+      api_hash,
+    });
+
+    // 3. Get the user country code
+    mtproto.call('help.getNearestDc', {}).then(result => {
+      console.log(`country:`, result);
+    });
+  }, [])
+
   return (
     <div>Telespector</div>
   );
