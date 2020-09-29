63dab88bad21c9673a0fe33daf5f53453dca5d1d
Dmitry Ch <dmitry.ch@inquire.dev>
2020-09-29 17:52:52 +0300

    Add switching in input fields

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -8,39 +8,69 @@ interface GeoInfo {
   country: string
 }
 
+const inputFieldJSX = (value, func) => (
+  <input
+    className="phone-auth-input"
+    placeholder="Phone"
+    type="text"
+    onChange={e => func(e)}
+    value={value}
+  />
+)
+
+const AUTH_STAGES = [
+  {
+    title: 'Phone number',
+    actionTitle: 'Send code',
+    inputField: inputFieldJSX
+  },
+  {
+    title: 'Confirm code',
+    actionTitle: 'Confirm',
+    inputField: inputFieldJSX
+  }
+]
+
+enum AUTH_STAGES_INDEXES {
+  PHONE_STAGE,
+  CODE_STAGE
+}
+
 export const Auth = () => {
   const [phone, setPhone] = useState('')
+  const [code, setCode] = useState('');
+  const [stage, setStage] = useState(AUTH_STAGES_INDEXES.PHONE_STAGE);
+
   const mtproto = useSelector((state: State) => state.user.mtproto)
 
   useEffect(() => {
       mtproto?.call('help.getNearestDc', {}).then(result => {
         const geo = result as GeoInfo
 
-        console.log(result)
-
         setPhone(`+${countryTelephoneCode(geo.country)}`)
       })
   }, [mtproto])
 
-  const onChangePhone = useCallback(e => {
+  const onChangeField = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
-    setPhone(e.target.value)
-  }, [])
+
+    if (stage === AUTH_STAGES_INDEXES.PHONE_STAGE) setPhone(e.target.value)
+    else setCode(e?.target?.value)
+  }, [stage])
 
   const onNextClick = useCallback(async () => {
     if (mtproto) {
       const res = await mtproto.call('auth.sendCode', {
         phone_number: '9996621534',
-        settings: {
-          _: 'codeSettings',
-        },
       })
 
-      console.log(res)
+      setStage(AUTH_STAGES_INDEXES.CODE_STAGE);
     }
   }, [mtproto])
 
   if (!mtproto) return <MTProtoLoader />
+
+  const stageData = AUTH_STAGES[stage];
 
   return (
     <div className="auth-page">
@@ -52,20 +82,16 @@ export const Auth = () => {
             alt="logo"
           />
         </div>
-        <h1>Authorization</h1>
+        <h1>{stageData.title}</h1>
         <div className="auth-form">
           <div>
-            <input
-              className="phone-auth-input"
-              placeholder="Phone"
-              type="text"
-              onChange={e => onChangePhone(e)}
-              value={phone}
-            />
+            {(stage === AUTH_STAGES_INDEXES.PHONE_STAGE) ?
+              stageData.inputField(phone, onChangeField) :
+              stageData.inputField(code, onChangeField)}
           </div>
           <div className="next-button" onClick={onNextClick}>
             <div className="next-button-content">
-              Send code<i className="material-icons next-button-icon">navigate_next</i>
+              {stageData.actionTitle}<i className="material-icons next-button-icon">navigate_next</i>
             </div>
           </div>
         </div>
