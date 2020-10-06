751d6ee1d08195dce3bc5502ebdc21a5c5cf1d08
destimon <destimon@github.com>
2020-10-07 01:02:49 +0300

    FE-2 Basic markup for home, styles for auth

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,8 +2,47 @@ import React from 'react'
 
 interface Props {}
 
+const menuMock = [
+  {
+    title: 'Lorem',
+    description: 'Lorem ipsum',
+  },
+  {
+    title: 'Lorem',
+    description: 'Lorem ipsum',
+  },
+  {
+    title: 'Lorem',
+    description: 'Lorem ipsum',
+  },
+  {
+    title: 'Lorem',
+    description: 'Lorem ipsum',
+  },
+  {
+    title: 'Lorem',
+    description: 'Lorem ipsum',
+  },
+]
+
 const Home = (props: Props) => {
-  return <div className="home-container">Hello</div>
+  return (
+    <div className="home-container">
+      <div className="top-head-content">
+        <div className="main-info-bar">
+          <img className="user-avatar" src="default_avatar.png" alt="" />
+          <div className="user-short-info">
+            <div className="main-user-info">@username</div>
+            <div className="sub-user-info">Dmitry Cherendieiev</div>
+          </div>
+        </div>
+      </div>
+      <div className="menu-bar">
+        <div className="menu-header">Tools & Preferences</div>
+        <div className="menu-content">div.</div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
