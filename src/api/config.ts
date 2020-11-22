119a54fe40c619ce36b07da159d9febcb8d28b2e
destimon <destaom@gmail.com>
2020-11-22 04:56:40 +0200

    Add env host name

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+console.log(process.env.REACT_APP_API_HOST_URL)
+
 const service = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: process.env.REACT_APP_API_HOST_URL,
 })
 
 export default service
--- src/api/telegramApi.ts
+++ src/api/telegramApi.ts
@@ -5,3 +5,5 @@ export const mtproto = new MTProto({
   api_hash: process.env.REACT_APP_TG_API_HASH || '-1',
   test: false,
 })
+
+// class
